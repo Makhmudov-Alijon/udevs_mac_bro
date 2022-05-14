5ad43e10e1722e1ac58b3fae0b19223974a4b18d
Makhmudov Alijon <98184267+alijonmakhmudov@users.noreply.github.com>
2022-05-14 11:06:46 +0500

    alijon

--- lib/ui/main/home/category_child_allitem/category_child_allitem_logic.dart
+++ lib/ui/main/home/category_child_allitem/category_child_allitem_logic.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:udevs_mac_bro/controller/home_controller.dart';
-import 'package:udevs_mac_bro/model/category_model/category_child_item.dart';
+
 import 'package:udevs_mac_bro/model/category_model/category_chils.dart';
 import 'package:udevs_mac_bro/repository/api_repository/api_repository.dart';
 
--- lib/ui/main/profil/profil.dart
+++ lib/ui/main/profil/profil.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+
 
 class ProfilPage extends StatelessWidget {
    ProfilPage({Key? key}) : super(key: key);
